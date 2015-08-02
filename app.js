bf023b74359798c4422dcbdc910b88cfce2fb768
Ben Sarmiento <me@bensarmiento.com>
2015-08-02 09:00:46 +0800

    random color generator

--- app.js
+++ app.js
@@ -3,10 +3,6 @@
 
   var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game');
 
-  game.state.add('Game', Game);
-
-  game.state.start('Game');
-
   var Game = {
     preload: function() {
 
@@ -17,11 +13,42 @@
     create: function() {
 
       // game.add.sprite(0, 0, 'star');
+      var graphics = game.add.graphics(0, 0);
+
+      // 0x44B3C2
+      // 0xF1A94E
+      // 0xE45641
+      // 0x5D4C46
+      // 0x7B8D8E
+      // 0xF2EDD8
+
+      var color = this.randomColor();
+      graphics.beginFill(color);
+      graphics.lineStyle(1, color);
+      graphics.drawRect(0, 0, 50, 50);
+
+      console.log('Heyyyyy');
 
     },
 
     update: function() {
       //
     },
+
+    randomColor: function() {
+      return this.colors[Math.floor(Math.random() * this.colors.length)];
+    },
+
   };
+
+  Game.colors = [
+    0x468966,
+    0xFFF0A5,
+    0xFFB03B,
+    0xB64926,
+  ];
+
+  game.state.add('Game', Game);
+  game.state.start('Game');
+
 })();
